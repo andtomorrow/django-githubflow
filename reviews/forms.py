39a99ef63b53b927eb9ebd1f0d62045b6bc76470
jangjaeyong1115 <leoangella12@naver.com>
2023-04-14 16:36:48 +0900

    final

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,13 +5,17 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ('title','content','movie',)
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+            'movie': forms.Textarea(attrs={'class': 'form-control', 'rows' : 1 }),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
-        widget = forms.Textarea(
-            attrs={
-                'class' : 'form-control row-1',
-            }
-        )
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
+        }
