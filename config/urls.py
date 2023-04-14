1a9c6a22b68d596c2f25d6756a1f292a8fead820
jangjaeyong1115 <leoangella12@naver.com>
2023-04-14 10:41:48 +0900

    required

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reviews/', include('reviews.urls')),
+    path('', include('reviews.urls')),
     path('accounts/', include('accounts.urls')),
 ]
